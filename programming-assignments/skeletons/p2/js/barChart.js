d9b8aaaba5453b101545ad94d66011a90f3e4f7a
fmnguyen <fm.nguyen937@gmail.com>
2023-10-19 01:47:21 -0700

    P2: Add a bit more structure to all of the js files, and spot check assignment.md

--- programming-assignments/skeletons/p2/js/barChart.js
+++ programming-assignments/skeletons/p2/js/barChart.js
@@ -1,18 +1,38 @@
 class BarChart {
 
-  constructor() {
-    
+  /**
+   * Class constructor with initial configuration
+   * @param {Object}
+   */
+  // Todo: Add or remove parameters from the constructor as needed
+  constructor(_config) {
+    this.config = {
+      parentElement: _config.parentElement,
+      containerWidth: 240,
+      containerHeight: 260,
+      margin: {
+        top: 30,
+        right: 5,
+        bottom: 20,
+        left: 30
+      }
+      // Todo: Add or remove attributes from config as needed
+    }
+    this.initVis();
   }
-  
+
   initVis() {
-    // Create SVG area, initialize scales and axes
+    let vis = this;
+    // Todo: Create SVG area, initialize scales and axes
   }
 
   updateVis() {
-    // Prepare data and scales
+    let vis = this;
+    // Todo: Prepare data and scales
   }
 
   renderVis() {
-    // Bind data to visual elements, update axes
+    let vis = this;
+    // Todo: Bind data to visual elements, update axes
   }
 }
--- programming-assignments/skeletons/p2/js/lexisChart.js
+++ programming-assignments/skeletons/p2/js/lexisChart.js
@@ -3,8 +3,8 @@ class LexisChart {
   /**
    * Class constructor with initial configuration
    * @param {Object}
-   * @param {Array}
    */
+  // Todo: Add or remove parameters from the constructor as needed
   constructor(_config) {
     this.config = {
       parentElement: _config.parentElement,
@@ -16,15 +16,17 @@ class LexisChart {
         bottom: 20,
         left: 25
       }
+      // Todo: Add or remove attributes from config as needed
     }
     this.initVis();
   }
 
-  /**
-   * Create scales, axes, and append static elements
-   */
   initVis() {
     let vis = this;
+
+    // Calculate inner chart size. Margin specifies the space around the actual chart.
+    vis.config.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+    vis.config.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
 
     // Define size of SVG drawing area
     vis.svg = d3.select(vis.config.parentElement)
@@ -36,12 +38,21 @@ class LexisChart {
     vis.chartArea = vis.svg.append('g')
       .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
 
-    vis.chart = vis.chartArea.append('g');
+    // Apply clipping mask to 'vis.chart' to clip arrows
+    vis.chart = vis.chartArea.append('g')
+      .attr('clip-path', 'url(#chart-mask)');
+
+    // Initialize clipping mask that covers the whole chart
+    vis.chart.append('defs')
+      .append('clipPath')
+      .attr('id', 'chart-mask')
+      .append('rect')
+      .attr('width', vis.config.width + 5)
+      .attr('y', -vis.config.margin.top)
+      .attr('height', vis.config.containerHeight);
 
     // Helper function to create the arrows and styles for our various arrow heads
     vis.createMarkerEnds();
-
-
 
     // Todo: initialize scales, axes, static elements, etc.
   }
@@ -49,22 +60,23 @@ class LexisChart {
 
   updateVis() {
     let vis = this;
-
     // Todo: prepare data
-
-    vis.renderVis();
   }
 
 
   renderVis() {
-
+    let vis = this;
     // Todo: Bind data to visual elements (enter-update-exit or join)
-
   }
 
-  // Create all of the different arrow heads. Styles: default, hover, highlight, highlight-selected
-  // To switch between these styles you can switch between the CSS class. See link below for more info.
-  // https://observablehq.com/@stvkas/interacting-with-marker-ends
+  /**
+   * Create all of the different arrow heads.
+   * Styles: default, hover, highlight, highlight-selected
+   * To switch between these styles you can switch between the CSS class.
+   * We populated an example css class with how to use the marker-end attribute.
+   * See link for more info.
+   * https://observablehq.com/@stvkas/interacting-with-marker-ends
+   */
   createMarkerEnds() {
     // Default arrow head
     // id: arrow-head
